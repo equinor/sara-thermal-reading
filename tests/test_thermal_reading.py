9269ae40dc210f05bf7d2e6b9e274fcc6f90d675
Ola Alstad <OALST@equinor.com>
2025-12-22 19:26:09 +0100

    Update imports and small fixes

--- tests/test_thermal_reading.py
+++ tests/test_thermal_reading.py
@@ -1,11 +1,13 @@
 import pytest
 
-from sara_thermal_reading.main import align_two_images
+from sara_thermal_reading.image_alignment.align_two_images_orb_bf_cv2 import (
+    align_two_images_orb_bf_cv2,
+)
 
 
-def test_align_two_images():
+def test_align_two_images() -> None:
     # This is a placeholder test. Replace with actual test logic.
-    # result = align_two_images(
+    # result = align_two_images_orb_bf_cv2(
     #     "tests/sample_image.png",
     #     "tests/sample_image2.png",
     #     [(100, 100), (200, 100), (200, 200), (100, 200)],
